0e7940c29c65043fa29ecb4d08155f4c5bbbec91
Unknown <winstonnau007@gmail.com>
2020-08-12 13:41:06 +0200

    :sparkles: added canvas drawing

--- App.tsx
+++ App.tsx
@@ -1,12 +1,143 @@
-import React, {useRef, Component} from 'react';
-import {Animated, StyleSheet, Button, View} from 'react-native';
+import React, {useRef} from 'react';
+import {
+  Animated,
+  StyleSheet,
+  Button,
+  View,
+  AppRegistry,
+  Text,
+} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import RNSketchCanvas from '@terrylinla/react-native-sketch-canvas';
 
 interface Main {
   props: any;
   navigation: any;
 }
+
+const Drawing = () => (
+  <View style={styles.container}>
+    <View style={{flex: 1, flexDirection: 'row'}}>
+      <RNSketchCanvas
+        containerStyle={{backgroundColor: 'transparent', flex: 1}}
+        canvasStyle={{backgroundColor: 'transparent', flex: 1}}
+        defaultStrokeIndex={0}
+        defaultStrokeWidth={5}
+        closeComponent={
+          <View style={styles.functionButton}>
+            <Text style={styles.white}>Close</Text>
+          </View>
+        }
+        undoComponent={
+          <View style={styles.functionButton}>
+            <Text style={styles.white}>Undo</Text>
+          </View>
+        }
+        clearComponent={
+          <View style={styles.functionButton}>
+            <Text style={styles.white}>Clear</Text>
+          </View>
+        }
+        eraseComponent={
+          <View style={styles.functionButton}>
+            <Text style={styles.white}>Eraser</Text>
+          </View>
+        }
+        strokeComponent={(color) => (
+          <View style={[{backgroundColor: color}, styles.strokeColorButton]} />
+        )}
+        strokeSelectedComponent={(color) => {
+          return (
+            <View
+              style={[
+                {backgroundColor: color, borderWidth: 2},
+                styles.strokeColorButton,
+              ]}
+            />
+          );
+        }}
+        strokeWidthComponent={(w) => {
+          return (
+            <View style={styles.strokeWidthButton}>
+              <View style={style(w).default} />
+            </View>
+          );
+        }}
+        saveComponent={
+          <View style={styles.functionButton}>
+            <Text style={styles.white}>Save</Text>
+          </View>
+        }
+        savePreference={() => {
+          return {
+            folder: 'RNSketchCanvas',
+            filename: String(Math.ceil(Math.random() * 100000000)),
+            transparent: false,
+            imageType: 'png',
+          };
+        }}
+      />
+    </View>
+  </View>
+);
+
+const style = (w: number) =>
+  StyleSheet.create({
+    default: {
+      backgroundColor: 'white',
+      marginHorizontal: 2.5,
+      width: Math.sqrt(w / 3) * 10,
+      height: Math.sqrt(w / 3) * 10,
+      borderRadius: (Math.sqrt(w / 3) * 10) / 2,
+    },
+  });
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  strokeColorButton: {
+    marginHorizontal: 2.5,
+    marginVertical: 8,
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+  },
+  strokeWidthButton: {
+    marginHorizontal: 2.5,
+    marginVertical: 8,
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#39579A',
+  },
+  functionButton: {
+    marginHorizontal: 2.5,
+    marginVertical: 8,
+    height: 30,
+    width: 60,
+    backgroundColor: '#39579A',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  playbuttonview: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  white: {
+    color: 'white',
+  },
+});
+
+AppRegistry.registerComponent('Drawing', () => Drawing);
 
 function MainScreen({navigation}: Main) {
   return (
@@ -16,9 +147,7 @@ function MainScreen({navigation}: Main) {
   );
 }
 
-function GameScreen({}: Main) {
-  return <View />;
-}
+const GameScreen = () => <Drawing />;
 
 const FadeInView = (props: any) => {
   const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
@@ -41,14 +170,6 @@ const FadeInView = (props: any) => {
     </Animated.View>
   );
 };
-
-const styles = StyleSheet.create({
-  playbuttonview: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 const Stack = createStackNavigator();
 

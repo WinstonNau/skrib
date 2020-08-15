9c5f665b61d258d27881b61b4749123fcc3ae7cd
Winston <winstonnau007@gmail.com>
2020-08-15 08:57:41 +0200

    :art: format the code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
-import React, { memo } from 'react';
-import { StyleSheet, Text } from 'react-native';
-import { theme } from '../core/theme';
+import React, {memo} from 'react';
+import {StyleSheet, Text} from 'react-native';
+import {theme} from '../core/theme';
 
 type Props = {
   children: React.ReactNode;
 };
 
-const Header = ({ children }: Props) => (
+const Header = ({children}: Props) => (
   <Text style={styles.header}>{children}</Text>
 );
 

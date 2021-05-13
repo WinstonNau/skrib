e8aaa92da6fef737af58820899932f385a9d4318
Winston <winstonnau007@gmail.com>
2021-05-13 10:05:11 +0200

    :sparkles: added voice recognition and word selection

--- src/screens/Game.tsx
+++ src/screens/Game.tsx
@@ -3,7 +3,160 @@ import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import RNSketchCanvas from '@terrylinla/react-native-sketch-canvas';
 import socket from '../lib/socket';
 
-let drawer = false;
+//TODO: Create a clock, so the drawer and the guessers can see the remaining time
+
+type Props = {};
+type State = {
+  recognized: string;
+  pitch: string;
+  error: string;
+  end: string;
+  started: string;
+  results: string[];
+  partialResults: string[];
+};
+
+class VoiceGuess extends Component<Props, State> {
+  state = {
+    recognized: '',
+    pitch: '',
+    error: '',
+    end: '',
+    started: '',
+    results: [],
+    partialResults: [],
+    resultWord: '',
+    stop: false,
+  };
+
+  constructor(props: Props) {
+    super(props);
+    Voice.onSpeechStart = this.onSpeechStart;
+    Voice.onSpeechEnd = this.onSpeechEnd;
+    Voice.onSpeechError = this.onSpeechError;
+    Voice.onSpeechResults = this.onSpeechResults;
+    Voice.onSpeechPartialResults = this.onSpeechPartialResults;
+  }
+
+  componentWillUnmount() {
+    Voice.destroy().then(Voice.removeAllListeners);
+  }
+
+  onSpeechStart = (e: any) => {
+    console.log('onSpeechStart: ', e);
+    this.state.stop = false;
+    this.setState({
+      started: '√',
+    });
+  };
+
+  // onSpeechRecognized = (e: SpeechRecognizedEvent) => {
+  //   console.log('onSpeechRecognized: ', e);
+  //   this.setState({
+  //     recognized: '√',
+  //   });
+  // };
+
+  onSpeechEnd = (e: any) => {
+    console.log('onSpeechEnd: ', e);
+    this.setState({
+      end: '√',
+    });
+  };
+
+  onSpeechError = (e: SpeechErrorEvent) => {
+    console.log('onSpeechError: ', e);
+    this.setState({
+      error: JSON.stringify(e.error),
+    });
+  };
+
+  onSpeechResults = (e: SpeechResultsEvent) => {
+    //TODO: Give feedback to the player if the guess was correct or not
+    //@ts-ignore
+    let w = e.value[0];
+    if (this.state.stop) {
+      if (w === chosenWord) {
+        console.log('correct guess');
+        showMessage({
+          message: w,
+          type: 'success',
+        });
+        socket.emit('correctGuess', gameIdG, playerUsernameG);
+        //TODO: Also call a mutation, which increases the score of the gamePlayer by 1
+      } else {
+        console.log('wrong guess');
+        showMessage({
+          message: w,
+          type: 'danger',
+        });
+        socket.emit('wrongGuess', gameIdG, w);
+      }
+      //@ts-ignore
+      console.log('onSpeechResults: ', e.value[0]);
+      this.setState({
+        results: e.value || [],
+      });
+    }
+  };
+
+  onSpeechPartialResults = (e: SpeechResultsEvent) => {
+    console.log('onSpeechPartialResults: ', e);
+    this.setState({
+      partialResults: e.value || [],
+    });
+  };
+
+  // onSpeechVolumeChanged = (e: any) => {
+  //   console.log('onSpeechVolumeChanged: ', e);
+  //   this.setState({
+  //     pitch: e.value,
+  //   });
+  // };
+
+  _startRecognizing = async () => {
+    this.setState({
+      recognized: '',
+      pitch: '',
+      error: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      end: '',
+    });
+
+    try {
+      await Voice.start('de-DE');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  _stopRecognizing = async () => {
+    try {
+      await Voice.stop();
+
+      this.state.stop = true;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.containerTwo}>
+        <TouchableOpacity
+          onPressIn={this._startRecognizing}
+          onPressOut={this._stopRecognizing}>
+          <Image
+            style={styles.button}
+            source={require('../assets/button.png')}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 
 const Drawing = () => {
   let canvas: RNSketchCanvas | null = null;
@@ -27,7 +180,84 @@ const Drawing = () => {
   if (drawer) {
     return (
       <SafeAreaView style={styles.container}>
+        <FlashMessage position="top" />
         <View style={{flex: 1, flexDirection: 'row'}}>
+          <Modal
+            testID={'modalSettings'}
+            isVisible={isWordModalVisible}
+            backdropColor="#B4B3DB"
+            backdropOpacity={0.8}
+            animationIn="zoomInDown"
+            animationOut="zoomOutUp"
+            animationInTiming={600}
+            animationOutTiming={600}
+            backdropTransitionInTiming={600}
+            backdropTransitionOutTiming={600}>
+            <View style={styles.content}>
+              <Text style={styles.contentTitle}>Choose a word:</Text>
+              <CountdownCircleTimer
+                isPlaying
+                duration={15}
+                colors={[
+                  ['#004777', 0.4],
+                  ['#F7B801', 0.4],
+                  ['#A30000', 0.2],
+                ]}
+                onComplete={() => {
+                  if (isWordModalVisible) {
+                    let word = '';
+                    switch (Math.floor(Math.random() * 3)) {
+                      case 0:
+                        word = choiceOneWord;
+                        break;
+                      case 1:
+                        word = choiceTwoWord;
+                        break;
+                      case 2:
+                        word = choiceThreeWord;
+                        break;
+                    }
+                    console.log(word);
+                    socket.emit('selectWord', gameIdG, word);
+                    setWordModalVisibility(false);
+                    startTimer(word);
+                  }
+                }}>
+                {({remainingTime, animatedColor}) => (
+                  <Animated.Text style={{color: animatedColor}}>
+                    {remainingTime}
+                  </Animated.Text>
+                )}
+              </CountdownCircleTimer>
+              <Button
+                testID={'choice-one-button'}
+                onPress={() => {
+                  socket.emit('selectWord', gameIdG, choiceOneWord);
+                  toggleChooseWordModal();
+                  startTimer(choiceOneWord);
+                }}>
+                {choiceOneWord}
+              </Button>
+              <Button
+                testID={'choice-two-button'}
+                onPress={() => {
+                  socket.emit('selectWord', gameIdG, choiceTwoWord);
+                  toggleChooseWordModal();
+                  startTimer(choiceTwoWord);
+                }}>
+                {choiceTwoWord}
+              </Button>
+              <Button
+                testID={'choice-three-button'}
+                onPress={() => {
+                  socket.emit('selectWord', gameIdG, choiceThreeWord);
+                  toggleChooseWordModal();
+                  startTimer(choiceThreeWord);
+                }}>
+                {choiceThreeWord}
+              </Button>
+            </View>
+          </Modal>
           <RNSketchCanvas
             onStrokeEnd={(path) => {
               console.log('onStrokeEnd:', JSON.stringify(path));
@@ -81,14 +311,6 @@ const Drawing = () => {
                 </View>
               );
             }}
-            // savePreference={() => {
-            //   return {
-            //     folder: 'RNSketchCanvas',
-            //     filename: String(Math.ceil(Math.random() * 100000000)),
-            //     transparent: false,
-            //     imageType: 'png',
-            //   };
-            // }}
           />
         </View>
       </SafeAreaView>
@@ -96,70 +318,32 @@ const Drawing = () => {
   } else {
     return (
       <SafeAreaView style={styles.container}>
+        <FlashMessage position="top" />
         <View style={{flex: 1, flexDirection: 'row'}}>
-          <RNSketchCanvas
-            // onStrokeEnd={(path) => {
-            //   console.log('onStrokeEnd:', JSON.stringify(path));
-            //   socket.emit('newPathDrawn', path);
-            // }}
-            ref={(ref: RNSketchCanvas) => (canvas = ref)}
-            //TODO: Figure out if every 4 commented methods are needed to be commented in order to not being able to draw on the canvas
-            // containerStyle={{backgroundColor: 'transparent', flex: 1}}
-            // canvasStyle={{backgroundColor: 'transparent', flex: 1}}
-            // defaultStrokeIndex={0}
-            // defaultStrokeWidth={5}
-            // undoComponent={
-            //   <View style={styles.functionButton}>
-            //     <Text style={styles.white}>Undo</Text>
-            //   </View>
-            // }
-            onUndoPressed={() => {
-              socket.emit('undoReq');
-            }}
-            // clearComponent={
-            //   <View style={styles.functionButton}>
-            //     <Text style={styles.white}>Clear</Text>
-            //   </View>
-            // }
-            onClearPressed={() => {
-              socket.emit('clearReq');
-            }}
-            // eraseComponent={
-            //   <View style={styles.functionButton}>
-            //     <Text style={styles.white}>Eraser</Text>
-            //   </View>
-            // }
-            // strokeComponent={(color) => (
-            //   <View
-            //     style={[{backgroundColor: color}, styles.strokeColorButton]}
-            //   />
-            // )}
-            // strokeSelectedComponent={(color) => {
-            //   return (
-            //     <View
-            //       style={[
-            //         {backgroundColor: color, borderWidth: 2},
-            //         styles.strokeColorButton,
-            //       ]}
-            //     />
-            //   );
-            // }}
-            // strokeWidthComponent={(w) => {
-            //   return (
-            //     <View style={styles.strokeWidthButton}>
-            //       <View style={style(w).default} />
-            //     </View>
-            //   );
-            // }}
-            // savePreference={() => {
-            //   return {
-            //     folder: 'RNSketchCanvas',
-            //     filename: String(Math.ceil(Math.random() * 100000000)),
-            //     transparent: false,
-            //     imageType: 'png',
-            //   };
-            // }}
-          />
+          <Modal
+            testID={'modalSettings'}
+            isVisible={isWaitingModalVisible}
+            backdropColor="#B4B3DB"
+            backdropOpacity={0.8}
+            animationIn="zoomInDown"
+            animationOut="zoomOutUp"
+            animationInTiming={600}
+            animationOutTiming={600}
+            backdropTransitionInTiming={600}
+            backdropTransitionOutTiming={600}>
+            <View style={styles.content}>
+              <Text style={styles.contentTitle}>Waiting...</Text>
+              <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+          </Modal>
+          <View style={{flex: 1}} pointerEvents={'none'}>
+            <RNSketchCanvas
+              ref={(ref: RNSketchCanvas) => (canvas = ref)}
+              containerStyle={{backgroundColor: 'transparent', flex: 1}}
+              canvasStyle={{backgroundColor: 'transparent', flex: 1}}
+            />
+          </View>
+          <VoiceGuess />
         </View>
       </SafeAreaView>
     );
@@ -213,6 +397,51 @@ const styles = StyleSheet.create({
   },
   white: {
     color: '#FFFFFF',
+  },
+  content: {
+    backgroundColor: 'white',
+    padding: 22,
+    justifyContent: 'center',
+    // alignItems: 'center',
+    borderRadius: 4,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  contentTitle: {
+    fontSize: 30,
+    marginBottom: 12,
+  },
+  button: {
+    width: 50,
+    height: 50,
+  },
+  containerTwo: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  action: {
+    textAlign: 'center',
+    color: '#0000FF',
+    marginVertical: 5,
+    fontWeight: 'bold',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  stat: {
+    textAlign: 'center',
+    color: '#B0171F',
+    marginBottom: 1,
   },
 });
 
